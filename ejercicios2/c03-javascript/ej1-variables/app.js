f81aac853e299a9c53278d3fd975e4615c0ffe72
omar-gitH <pmardlwwfke@gmail.com>
2026-04-15 20:05:46 -0300

    duplicados

---
+++ ejercicios2/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,19 @@
+const nombre = "Omar";
+const edad = 20;
+const materia = "Desarrollo de Software";
+
+const mensaje = `Hola, mi nombre es ${nombre}, tengo ${edad} años y estoy estudiando ${materia}.`;
+console.log(mensaje);
+
+let contador = 0;
+console.log(`El contador vale ${contador}`);
+contador ++;
+contador ++;
+contador ++;
+console.log(`El contador vale ${contador}`);
+
+for (let i = 0; i < 20; i++) {
+    const valor = `el valor de i es ${i}`;
+    console.log(valor);
+}
+
